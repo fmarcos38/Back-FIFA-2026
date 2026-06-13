611d45062e3227d65f22c6563f42e444c71d7fad
fmarcos38 <fmarcos_23@hotmail.com>
2026-06-13 15:25:52 -0300

    deploy

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,9 +3,7 @@ const { adminToken } = require('../config/authToken')
 
 const login = (req, res) => {
   const { username, password } = req.body
-  const valid =
-    (username === adminUser.username && password === adminUser.password) ||
-    (username === 'admin' && password === 'admin2026')
+  const valid = username === adminUser.username && password === adminUser.password
 
   if (!valid) {
     return res.status(401).json({ ok: false, message: 'Credenciales incorrectas' })
